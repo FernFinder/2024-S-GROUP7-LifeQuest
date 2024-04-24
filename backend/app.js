8213834eecbf882c06e736b3369ba426afb603fd
FernFinder <130785206+FernFinder@users.noreply.github.com>
2024-04-24 14:57:28 -0700

    Removed unused route.

--- backend/app.js
+++ backend/app.js
@@ -62,7 +62,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Routing for pages
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/settings', settingsRouter)
 
 
 // catch 404 and forward to error handler
